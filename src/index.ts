a04302a3b2f454d6852fb41da92743db2c14e5f5
YTDavid93 <ytd3809@gmail.com>
2024-08-09 20:41:15 +0545

    build api to get all movies genres

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import express, { Express, Request, Response} from "express";
+import express, { Express, Request, Response } from "express";
+import { get } from "http";
 
 const app: Express = express();
 
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "Drama" },
+];
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + Typescript server");
+  res.send("Express + Typescript server");
+});
+
+app.get("/api/genres", (req: Request, res: Response) => {
+    res.send(genres);
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Listening on Port:${port}`);
-})
+  console.log(`Listening on Port:${port}`);
+});
