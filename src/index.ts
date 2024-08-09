61e352389fbdbd4b8475cca9d0b802ff09fce6d0
YTDavid93 <ytd3809@gmail.com>
2024-08-09 20:56:12 +0545

    build api to get single course

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Express, Request, Response } from "express";
-import { get } from "http";
 
 const app: Express = express();
+
+app.use(express.json())
 
 const genres = [
   { id: 1, name: "Action" },
@@ -15,6 +16,13 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/api/genres", (req: Request, res: Response) => {
     res.send(genres);
+})
+
+// to get single course
+app.get("/api/genres/:id", (req: Request, res: Response) => {
+    const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send("The genre with the given Id was not found.");
+    res.send(genre);
 })
 
 const port = process.env.PORT || 3000;
