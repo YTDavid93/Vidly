b1f865eaeb65685b98d81787710b157b6847a503
YTDavid93 <ytd3809@gmail.com>
2024-08-09 21:19:45 +0545

    handle http post request

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { error } from "console";
 import express, { Express, Request, Response } from "express";
+import { z } from "zod";
+
 
 const app: Express = express();
+app.use(express.json());
 
-app.use(express.json())
-
-const genres = [
+interface Genre {
+    id: number;
+    name: string;
+}
+ 
+const genres: Genre[] = [
   { id: 1, name: "Action" },
   { id: 2, name: "Comedy" },
   { id: 3, name: "Drama" },
@@ -25,8 +32,31 @@ app.get("/api/genres/:id", (req: Request, res: Response) => {
     res.send(genre);
 })
 
+// Handaling the HTTP post request
+app.post("/api/genres", (req: Request, res: Response) => {
+  
+   const { error } = validateGenre(req.body)
+   if (error) return res.status(400).json({ errors: error.errors })
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre);
+    res.send(genre);
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on Port:${port}`);
 });
+
+const validateGenre = (genre: Genre) => {
+  const schema = z.object({
+    name: z.string().min(1, { message: "Genre name is required" }),
+  });
+
+  return schema.safeParse(genre)
+}
