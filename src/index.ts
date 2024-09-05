d1740db0adf547565b67509ee7e041fa8b414b65
YTDavid93 <ytd3809@gmail.com>
2024-09-05 16:15:16 +0545

    mongoose connection code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Express } from "express";
+import mongoose from "mongoose";
 import router from "../routes/genres";
 import routerHome from "../routes/home";
 
 const app: Express = express();
 app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost/vidly-exercise")
+  .then(() => console.log("Connected to databse"))
+  .catch((err) => console.log("Couldn't connect to Mongodb", err));
+
+  
 
 const genres = router;
 const home = routerHome;
